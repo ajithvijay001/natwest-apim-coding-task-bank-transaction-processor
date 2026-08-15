13cf5b690dd1b562a6fdf0d82ec958cc17f8a22d
ajithvijay001 <ajith007.v@gmail.com>
2026-08-15 22:09:18 +0530

    chore: initial Spring Boot project setup

---
+++ bank-transaction-processor/src/main/java/com/natwest/bank_transaction_processor/BankTransactionProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.natwest.bank_transaction_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionProcessorApplication.class, args);
+	}
+
+}
