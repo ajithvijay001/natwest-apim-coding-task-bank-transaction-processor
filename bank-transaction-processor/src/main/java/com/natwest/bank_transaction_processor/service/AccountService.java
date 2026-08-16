5bde02025115df9736643a764f0a3037332e0fd0
ajithvijay001 <ajith007.v@gmail.com>
2026-08-17 05:27:05 +0530

    feat: implement deposit with validation for non-existent account and invalid amount

--- bank-transaction-processor/src/main/java/com/natwest/bank_transaction_processor/service/AccountService.java
+++ bank-transaction-processor/src/main/java/com/natwest/bank_transaction_processor/service/AccountService.java
@@ -22,4 +22,17 @@ public class AccountService {
 		
 		return newAccount; 
 	}
+	
+	public BigDecimal deposit(Long accountId, BigDecimal depositAmount) {
+		
+		if(depositAmount.compareTo(BigDecimal.ZERO)<=0) throw new IllegalArgumentException("Deposit amount should be greater than 0");
+		
+		if(!accountMap.containsKey(accountId)) throw new IllegalArgumentException("Account Id does not exists");
+		
+		Accounts account = accountMap.get(accountId);
+		account.setBalance(account.getBalance().add(depositAmount));
+		accountMap.put(accountId, account);
+		
+		return account.getBalance();
+	}
 }
