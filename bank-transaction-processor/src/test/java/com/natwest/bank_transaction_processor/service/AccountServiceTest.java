7a2bba2105daf6a9043e4426162730113f9c25bd
ajithvijay001 <ajith007.v@gmail.com>
2026-08-17 02:17:56 +0530

    test: add failing tests for account creation with valid and negative amounts

---
+++ bank-transaction-processor/src/test/java/com/natwest/bank_transaction_processor/service/AccountServiceTest.java
@@ -0,0 +1,34 @@
+package com.natwest.bank_transaction_processor.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+public class AccountServiceTest {
+	
+	private final AccountService accountService = new AccountService();
+
+	@Test
+	void createAccount_withValidAmount_setBalanceCorrectly() {
+		
+		String accountName = "John";
+		BigDecimal amount = new BigDecimal("10000.00");
+		
+		Accounts account = accountService.createAccount(accountName, amount);
+		
+		assertEquals(amount, account.getAvailableBalance());
+		assertNotNull(account.getAccountId());
+	}
+	
+	@Test
+	void createAccount_withNegativeAmount_throwsException() {
+		String accountName = "Mafi";
+		BigDecimal amount = new BigDecimal("-10000.00");
+		
+		assertThrows( IllegalArgumentException.class, () -> {accountService.createAccount(accountName, amount);});
+		
+	}
+}
