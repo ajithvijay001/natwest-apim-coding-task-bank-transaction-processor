b8fad873e61aeb62630ed169bd7e0cd6dc26fc71
ajithvijay001 <ajith007.v@gmail.com>
2026-08-17 16:35:59 +0530

    refactor: convert Transaction to a Java record for immutability

--- bank-transaction-processor/src/main/java/com/natwest/bank_transaction_processor/model/Transaction.java
+++ bank-transaction-processor/src/main/java/com/natwest/bank_transaction_processor/model/Transaction.java
@@ -5,52 +5,10 @@ import java.time.LocalDateTime;
 
 import com.natwest.bank_transaction_processor.model.enums.TransactionType;
 
-public class Transaction {
-	
-	private Long transactionId;
-	
-	private Long accountId;
-	
-	private TransactionType transactionType;
-	
-	private BigDecimal amount;
-	
-	private BigDecimal balance;
-	
-	private LocalDateTime transactionTimestamp;
-	
-	public Transaction(Long transactionId, Long accountId, TransactionType transactionType, BigDecimal amount, 
-			BigDecimal balance, LocalDateTime transactionTimestamp) {
-		this.transactionId = transactionId;
-		this.accountId = accountId;
-		this.transactionType = transactionType;
-		this.amount = amount;
-		this.balance = balance;
-		this.transactionTimestamp = transactionTimestamp;
-	}
-
-	public Long getTransactionId() {
-		return transactionId;
-	}
-
-	public Long getAccountId() {
-		return accountId;
-	}
-
-	public TransactionType getTransactionType() {
-		return transactionType;
-	}
-
-	public BigDecimal getAmount() {
-		return amount;
-	}
-
-	public BigDecimal getBalance() {
-		return balance;
-	}
-
-	public LocalDateTime getTransactionTimestamp() {
-		return transactionTimestamp;
-	}
-	
-}
+public record Transaction(
+		Long transactionId,
+		Long accountId, 
+		TransactionType transactionType,
+		BigDecimal amount,
+		BigDecimal balance,
+		LocalDateTime transactionTimestamp) {}
