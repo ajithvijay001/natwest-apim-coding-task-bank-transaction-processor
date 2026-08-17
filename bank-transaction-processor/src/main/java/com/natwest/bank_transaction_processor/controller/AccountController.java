fd1a6a2ee858bbb330fb846bf4f9a93eb8d1f27e
ajithvijay001 <ajith007.v@gmail.com>
2026-08-17 19:18:39 +0530

    feat: implement POST /accounts endpoint for account creation

---
+++ bank-transaction-processor/src/main/java/com/natwest/bank_transaction_processor/controller/AccountController.java
@@ -0,0 +1,28 @@
+package com.natwest.bank_transaction_processor.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.natwest.bank_transaction_processor.dto.CreateAccountRequest;
+import com.natwest.bank_transaction_processor.model.Accounts;
+import com.natwest.bank_transaction_processor.service.AccountService;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Accounts> createAccount(@RequestBody CreateAccountRequest request) {
+        Accounts account = accountService.createAccount(request.getAccountName(), request.getInitialBalance());
+        return ResponseEntity.ok(account);
+    }
+}
